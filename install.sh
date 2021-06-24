85d280ff3d666940d30b7542771469cf3277d974
Carlos Diaz-Padron <carlosdiazpadron@gmail.com>
2021-06-23 23:45:36 -0400

    fix links

--- install.sh
+++ install.sh
@@ -10,11 +10,12 @@ ln -s $DOTROOT/.zshrc $HOME/.zshrc
 ln -s $DOTROOT/.oh-my-zsh $HOME/.oh-my-zsh
 mkdir -p $HOME/.config
 ln -s $DOTROOT/.vim $HOME/.config/nvim
-# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 touch ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
-sudo apt update
-sudo apt install -y hub zsh-syntax-highlighting neovim
+if [ "$(uname)" != "Darwin" ]; then
+	sudo apt update
+	sudo apt install -y hub zsh-syntax-highlighting neovim
+fi
 nvim --headless +PlugInstall +qa
